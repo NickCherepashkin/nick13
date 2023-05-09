3e3340558edf33e314827dcb367839e82f2392e7
Nika Drozdova <cherepawka13@mail.ru>
2023-05-09 04:43:44 +0300

    check connection

--- app/src/main/java/com/drozdova/app/presentation/MainActivity.kt
+++ app/src/main/java/com/drozdova/app/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.drozdova.app.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.drozdova.app.R
 import com.drozdova.app.databinding.ActivityMainBinding
 import com.drozdova.app.presentation.view.MainFragment
@@ -10,6 +11,8 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var checkNetworkConnection: InternetConnection
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,5 +21,18 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .add(R.id.fragmentContainerView, MainFragment())
             .commit()
+
+        callNetworkConnection()
+    }
+
+    private fun callNetworkConnection() {
+        checkNetworkConnection = InternetConnection(application)
+        checkNetworkConnection.observe(this) { isConnected ->
+            if (isConnected) {
+                binding.layoutConnectionError.visibility = View.GONE
+            } else {
+                binding.layoutConnectionError.visibility = View.VISIBLE
+            }
+        }
     }
 }
